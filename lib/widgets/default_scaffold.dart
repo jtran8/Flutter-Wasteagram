dc6193d4ce58b3c8085d052ee6a3bc882c350786
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-09 21:21:50 -0500

    Create default_scaffold.dart

---
+++ lib/widgets/default_scaffold.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DefaultScaffold extends StatelessWidget {
+  final body;
+  final title;
+  final routeName;
+
+  DefaultScaffold({this.body, this.title, this.routeName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: this.body,
+      floatingActionButton: addButton(context)
+    );
+  }
+
+  FloatingActionButton addButton(BuildContext context) {
+    if (routeName != null) {
+      return FloatingActionButton(
+        onPressed: () => Navigator.of(context).pushNamed(this.routeName),
+        tooltip: 'Create a new post.',
+        child: const Icon(Icons.add),
+      );
+    }
+    return null;
+  }
+}
