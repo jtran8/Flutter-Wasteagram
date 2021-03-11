2d683a34a5da8b3ef61b69fca02da7ea17fe5137
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-11 01:24:19 -0500

    Update new_screen.dart

--- lib/screens/new_screen.dart
+++ lib/screens/new_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import '../widgets/default_scaffold.dart';
 
 class NewScreen extends StatefulWidget {
