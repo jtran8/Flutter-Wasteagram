d45595377e6ea90008a50bbf0e2178cd002542e9
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-13 01:12:04 -0500

    Update detail_screen.dart

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/post.dart';
 import '../widgets/default_scaffold.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -11,8 +12,9 @@ class DetailScreen extends StatefulWidget {
 class _PostScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
+    final FoodWastePost data = ModalRoute.of(context).settings.arguments;
     return DefaultScaffold(
-      title: 'Post Details',
+      title: '${data.date}',
       body: Text('Post Screen')
     );
   }
