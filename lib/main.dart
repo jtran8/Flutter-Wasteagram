a25c80f98695fe51ebeb1af1a5e4c247cab0f404
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-13 01:11:59 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app.dart';
@@ -9,5 +10,6 @@ void main() async {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
+  await Firebase.initializeApp();
   runApp(App());
 }
