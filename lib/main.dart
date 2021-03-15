af4e639fb3886951dee73ffcd26c297f8fa90fd2
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-14 23:11:25 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// Author: Jacky Tran
+// Program: CS492 Project 5: Wasteagram
+// Description: The main file for this app.
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
