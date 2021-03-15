4494f6a79f28ccc068d36070ecc8d9401ac9b7a5
jtran8 <40408767+jtran8@users.noreply.github.com>
2021-03-14 21:23:43 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'app.dart';
 
 void main() async {
@@ -11,5 +12,11 @@ void main() async {
     DeviceOrientation.landscapeRight,
   ]);
   await Firebase.initializeApp();
-  runApp(App());
+  // From: https://docs.sentry.io/platforms/flutter/
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://933eba7715fd44808418c588b581a173@o551699.ingest.sentry.io/5675369';
+    },
+    appRunner: () => runApp(MyApp()),
+  );
 }
